d4564ac7c99ffba846d3221619e31d52e427f138
Matthew Cuba <matthew.cuba@gmail.com>
2021-12-01 13:03:29 -0500

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    let lines = IO.File.ReadLines @"C:\adventofcode\01\input.txt"
+    let values = lines |> Seq.map System.Int32.Parse|> Seq.pairwise |> Seq.toList
+    let largerThan = values |> List.filter (fun (x,y) -> x < y)
+    printfn "Number of increases: %d" largerThan.Length
+
+    0 // return an integer exit code
